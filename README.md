81e331bc9b4ae1babd3f9b131a521199a1f1f4d3
Matheus Da Mata <matheusdamatag@gmail.com>
2024-05-18 17:12:01 -0300

    complete study

---
+++ README.md
@@ -0,0 +1,5 @@
+# Testes Spies
+
+Eles observam suas funções, validando a quantidades de vezes que ela foi chamada, com quais parâmetros e quais resultados elas retornaram em cada tempo.
+
+* Extremamente útil em cenários onde criamos funções recursivas ou que dependem de uma lógica complexa para um término.
---
+++ src/fibonacci.test.js
@@ -0,0 +1,39 @@
+const {createSandbox} = require('sinon')
+const assert = require('assert')
+const Fibonacci = require('./fibonacci')
+const sinon = createSandbox()
+
+;(async () => {
+  {
+    const fibonacci = new Fibonacci()
+    const spy = sinon.spy(
+      fibonacci,
+      fibonacci.execute.name
+    )
+
+    for (const sequencia of fibonacci.execute(3)) {} 
+
+    const expectedCallCount = 4
+    assert.strictEqual(spy.callCount, expectedCallCount)
+  }
+
+  {
+    const fibonacci = new Fibonacci()
+    const spy = sinon.spy(
+      fibonacci,
+      fibonacci.execute.name
+    )
+
+    const results = [...fibonacci.execute(5)]
+
+    const expectedCallCount = 6
+    assert.strictEqual(spy.callCount, expectedCallCount)
+
+    const { args } = spy.getCall(2)
+    const expectedParams = [3, 1, 2]
+    assert.deepStrictEqual(args, expectedParams) 
+
+    const expectedResuls = [0, 1, 1, 2, 3]
+    assert.deepStrictEqual(results, expectedResuls)
+  }
+})()
